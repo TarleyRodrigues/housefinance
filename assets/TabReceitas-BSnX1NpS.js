c198321ac7fafff83f8ec2d156bb45f618ab56f6
TarleyRodrigues <tarley.divino@gmail.com>
2026-06-18 10:31:47 -0300

    Updates

--- assets/TabReceitas-BSnX1NpS.js
+++ assets/TabReceitas-BSnX1NpS.js
@@ -1,4 +1,4 @@
-import{c as K,r as c,j as e,A as N,P as W,G as pe,m as j,k as ue,L as F,H as ge,z as fe,y as T,X as je,I as he,J as Ae,u as H,K as Y,N as $e}from"./index-DIi325Le.js";import{A as ke}from"./arrow-left-CkFtIwWn.js";import{P as Te}from"./pencil-s80Er5oo.js";import{P as Me}from"./pause-Bq7IYJMU.js";/**
+import{c as K,r as c,j as e,A as N,P as W,G as pe,m as j,k as ue,L as F,H as ge,z as fe,y as T,X as je,I as he,J as Ae,u as H,K as Y,N as $e}from"./index-DnsmGubB.js";import{A as ke}from"./arrow-left-CpIm8nzn.js";import{P as Te}from"./pencil-CxB783Hn.js";import{P as Me}from"./pause-Cf5rY9Sa.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
