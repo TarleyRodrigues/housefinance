fe18232dad63b8b7f7c8d7398c76085bed496fd9
TarleyRodrigues <tarley.divino@gmail.com>
2026-06-22 11:39:59 -0300

    Updates

--- assets/sparkles-B8LE8DEw.js
+++ assets/sparkles-B8LE8DEw.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B4-T3GzU.js";/**
+import{c as a}from"./index-CtYjKCJ7.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
